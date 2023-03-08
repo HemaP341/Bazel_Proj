096791a8add7a8b819ebce6d2c5551bae1e78189
HemaP341 <127285452+HemaP341@users.noreply.github.com>
2023-03-07 22:35:08 -0800

    Add files via upload

---
+++ examples/tests/function_tests.cpp
@@ -0,0 +1,13 @@
+#include "gtest/gtest.h"
+
+#include "examples/library/library.h"
+
+/// \file
+/// \author Jeff Barbieri
+/// \date Jun 30 2019
+
+/// Test design
+TEST(Algorithm_Tests, Example)
+{
+  ASSERT_EQ(function(1), 2);
+}
